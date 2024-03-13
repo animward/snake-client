9bbff6ae4a312e0139342157e7a388503bc04ced
EgoExperiments <egoexperiments@gmail.com>
2024-03-13 15:21:39 -0700

    setup interface

--- play.js
+++ play.js
@@ -1,4 +1,18 @@
 const connect = require("./client");
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+
+
+
 console.log("Connecting ...");
 connect();
+
