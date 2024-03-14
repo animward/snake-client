16bb9e8351e7c65895a378a41490e7138890b682
EgoExperiments <egoexperiments@gmail.com>
2024-03-13 17:44:12 -0700

    use constants in client.js

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP } = require("./constants");
 
 // Establishes a connection with the game server
 const connect = function (setupInput) {
     const conn = net.createConnection({
-        host: "165.227.47.243",
-        port: 50541
+        host: IP,
+        port: PORT
     });
 
     // Interpret incoming data as text
